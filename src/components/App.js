109e60d3d306a18d67844cb97293caa87bb11c6f
Samuel Aronda <arondasamuel123@gmail.com>
2020-11-03 17:26:25 +0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className=" mt-5">
+      <h3 className="text-black text-4xl">Welcome to the Turf4u App</h3>
+    </div>
+  )
+}
+
+export default App
